024d7645da1fa0ab3eaea96c1b19742d20d4dcfc
JorgeHabib <jorgehabib.khouri@gmail.com>
2025-06-23 19:16:53 -0300

    Add main application class for intelligent-antifraud project, initializing Spring Boot application.

---
+++ intelligent-antifraud/src/main/java/com/jorgehabib/intelligent_antifraud/IntelligentAntifraudApplication.java
@@ -0,0 +1,13 @@
+package com.jorgehabib.intelligent_antifraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentAntifraudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentAntifraudApplication.class, args);
+	}
+
+}
